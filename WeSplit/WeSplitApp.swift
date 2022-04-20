54d0760fa27d053f5bd53de9dd73a2ed830bcc92
Giuseppe <77759603+gsardo@users.noreply.github.com>
2022-04-20 17:38:43 +1000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Giuseppe Sardo on 20/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
